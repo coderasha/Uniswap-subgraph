9dc928e87042da6fb72b8b9f7f920daeacb94ddc
coderasha <ashadipta@gmail.com>
2024-03-04 13:03:41 +0530

    create client

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState,useEffect } from 'react'
+import {createClient} from "urql"
 
 import './App.css'
 
 function App() {
-
+  const QueryURL = "https://gateway.thegraph.com/api/6f9d8275fa98eef5c0b958d0a53842fc/subgraphs/id/ELUcwgpm14LKPLrBRuVvPvNKHQ9HvwmtKgKSH6123cr7"
+const query = '{}'
 
   return (
     <>
